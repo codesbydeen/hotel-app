c5e0271c6751569e82a56c67e2993a1481975dfa
Geraldine <c4dn5wld3@vossie.net>
2026-01-30 17:47:10 +0200

    Using localStorage in the reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,6 +7,11 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
+  constructor() {
+    let savedReservations = localStorage.getItem('reservations');
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
   getReservations(): Reservation[] {
     return this.reservations;
   }
@@ -17,7 +22,7 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
-    console.log('Reservation added:', reservation);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
@@ -25,6 +30,7 @@ export class ReservationService {
       (reservation) => reservation.id === id,
     );
     this.reservations.splice(index, 1);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   updateReservation(updatedReservation: Reservation): void {
@@ -32,5 +38,6 @@ export class ReservationService {
       (reservation) => reservation.id === updatedReservation.id,
     );
     this.reservations[index] = updatedReservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 }
